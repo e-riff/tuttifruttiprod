61c8f1d4309035e9fd230bd3cd0648bf8e8fcd5d
e-riff <emeric.riff@gmail.com>
2025-04-22 11:33:11 +0200

    feature(refacto1)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,8 +8,6 @@ use App\Service\MailerService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
@@ -19,7 +17,6 @@ class HomeController extends AbstractController
         Request         $request,
         MailerService   $contactMail,
         BandRepository  $bandRepository,
-        MailerInterface $mailer,
     ): Response
     {
         $contactForm = $this->createForm(MessageType::class);
